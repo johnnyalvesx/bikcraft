107c7028792354342eb89d7d82ba104963497c2d
johnnyalvesx <alvesjohnny@outlook.com>
2024-02-17 16:46:40 -0300

    ✨ Feat: guardar parametros url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativa links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,17 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativa itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
